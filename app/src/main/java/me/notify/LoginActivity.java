7616bdf76e48206a394809e7fdf26ca9db0183a4
Rommel David <rommel@gorated.com>
2015-08-08 15:14:33 +0800

    Added login page.

---
+++ app/src/main/java/me/notify/LoginActivity.java
@@ -0,0 +1,16 @@
+package me.notify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by David Rommel, B. on 8/8/15.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+    }
+}
